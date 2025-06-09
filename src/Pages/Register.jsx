8a4d432c4cc551c5d7c3a6f6dffa98f5d87cb3f9
SssamirAryal <166592937+SssamiAryal@users.noreply.github.com>
2025-06-09 08:37:30 +0545

    Form Updated

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "../assets/Register.css";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function Register() {
   const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const onSubmit = (data) => {
+    console.log(data);
     alert("Registered successfully!");
     navigate("/login");
   };
@@ -18,11 +24,30 @@ function Register() {
         <div className="register-box">
           <h2>Join JourneyTrekker!</h2>
           <p>Create your account to explore amazing travel deals.</p>
-          <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Full Name" required />
-            <input type="email" placeholder="Email Address" required />
-            <input type="password" placeholder="Create Password" required />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              placeholder="Full Name"
+              {...register("name", { required: "Name is required" })}
+            />
+            {errors.name && <p>{errors.name.message}</p>}
+
+            <input
+              type="email"
+              placeholder="Email Address"
+              {...register("email", { required: "Email is required" })}
+            />
+            {errors.email && <p>{errors.email.message}</p>}
+
+            <input
+              type="password"
+              placeholder="Create Password"
+              {...register("password", { required: "Password is required" })}
+            />
+            {errors.password && <p>{errors.password.message}</p>}
+
             <button type="submit">Register</button>
+
             <p className="login-link">
               Already have an account?{" "}
               <span onClick={() => navigate("/login")}>Login here</span>
